1ea8e9b29b1f35aa9edecbdf0208e8bec62f33b5
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2020-02-06 21:50:20 +0800

    login

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+
+function connDb(){
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        return "Connected successfully";
+}
